9e0d6d3ae4c88eccd56dc88fe4dc02acd2eceab5
jinnn888 <tuyor.jynchris@gmail.com>
2025-01-08 10:17:02 +0800

    fix: cors errors

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
 
       $token = $user->createToken('auth_token')->plainTextToken;
       return response()->json([
-        'data'          => $user,
+        'user'          => $user,
         'access_token'  => $token,
         'token_type'    => 'Bearer'
     ]);
